0b1894b9666b6e33475a116078784acdf14dff4d
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-28 10:52:18 +0200

    3.1 puhelinluettelon backend osa 1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      "name": "Martti Tienari",
+      "phone": "040 - 000000",
+      "id": 1
+    },
+    {
+      "name": "Arto Järvinen",
+      "phone": "040-123456",
+      "id": 3
+    },
+    {
+      "name": "Leidi Livia",
+      "phone": "040 - 8747474",
+      "id": 5
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Tervetuloa puhelinluetteloon!</h1>')
+  })
+
+
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
