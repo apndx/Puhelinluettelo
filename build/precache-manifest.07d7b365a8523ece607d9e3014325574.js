48616bd24726e38e5b409fb177f39883fd53dad1
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-30 17:06:02 +0200

    3.11 puhelinluettelo full stack

---
+++ build/precache-manifest.07d7b365a8523ece607d9e3014325574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dab7420d6131e4ea5671",
+    "url": "/static/js/main.dab7420d.chunk.js"
+  },
+  {
+    "revision": "3b1846dbd9e8365c15a3",
+    "url": "/static/js/1.3b1846db.chunk.js"
+  },
+  {
+    "revision": "dab7420d6131e4ea5671",
+    "url": "/static/css/main.de3a54ac.chunk.css"
+  },
+  {
+    "revision": "73abdaef41cb25ee34f82a415f070ef4",
+    "url": "/index.html"
+  }
+];
